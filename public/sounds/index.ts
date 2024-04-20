03a586505f4fd841cd8f5cd0f1fe53a669086ebf
ferutizi <fer.utizi@gmail.com>
2024-04-20 17:02:28 -0300

    added sounds, file-loader dependency, declare modules, and nextConfig

---
+++ public/sounds/index.ts
@@ -0,0 +1,4 @@
+export { default as red } from './red.mp3'
+export { default as blue } from './blue.mp3'
+export { default as yellow } from './yellow.mp3'
+export { default as green } from './green.mp3'
